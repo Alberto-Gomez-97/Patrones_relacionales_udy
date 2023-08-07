27a3220eb0db5b59d896a9d1198e3b74ab8f5a1c
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-06 18:53:41 -0600

    Add files via upload

---
+++ Patrones_relacionales_udy/Patrones_relacionales_udy/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//PATRONES RELACIONALES
+//para switches 
+var temperatura = 35;
+
+var mensaje = temperatura switch
+{
+    37 => "No tienes ninguna condicion",
+    > 37 => "Tienes fiebre",
+    < 37 => "Puede que tengas baja el azúcar"
+};
+Console.WriteLine(mensaje);
